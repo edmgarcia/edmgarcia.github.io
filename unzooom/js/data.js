1c6e8d2cc93b34a01d0e943855a5550c9ba3a501
Poi <65627716+edmgarcia@users.noreply.github.com>
2023-01-14 17:46:03 -0500

    Update data.js
    corrected image path

--- unzooom/js/data.js
+++ unzooom/js/data.js
@@ -1,163 +1,163 @@
 const data = [
 	{
 		id: 0,
-		img: "images/zoom/lightbulb.jpg",
+		img: "unzooom/images/zoom/lightbulb.jpg",
 		title: "Lightbulb",
 		buttons: ["Lightbulb", "Hotdog", "Space"]
 	},
 	{
 		id: 1,
-		img: "images/zoom/jellybeans.jpg",
+		img: "unzooom/images/zoom/jellybeans.jpg",
 		title: "Jellybeans",
 		buttons: ["Corals", "Jellybeans", "Marbles"]
 	},
 	{
 		id: 2,
-		img: "images/zoom/sun.jpg",
+		img: "unzooom/images/zoom/sun.jpg",
 		title: "Sun",
 		buttons: ["Sponge", "Sun", "Fire"]
 	},
 	{
 		id: 3,
-		img: "images/zoom/lemon.jpg",
+		img: "unzooom/images/zoom/lemon.jpg",
 		title: "Lemon",
 		buttons: ["Lemon", "Yellow Bell", "Steak"]
 	},
 	{
 		id: 4,
-		img: "images/zoom/bowling.jpg",
+		img: "unzooom/images/zoom/bowling.jpg",
 		title: "Bowling",
 		buttons: ["Wood", "Candy", "Bowling"]
 	},
 	{
 		id: 5,
-		img: "images/zoom/pig.jpg",
+		img: "unzooom/images/zoom/pig.jpg",
 		title: "Pig",
 		buttons: ["Tree", "Soil", "Pig"]
 	},
 	{
 		id: 6,
-		img: "images/zoom/coconut.jpg",
+		img: "unzooom/images/zoom/coconut.jpg",
 		title: "Coconut",
 		buttons: ["Trunk", "Coconut", "Cloud"]
 	},
 	{
 		id: 7,
-		img: "images/zoom/gum.jpg",
+		img: "unzooom/images/zoom/gum.jpg",
 		title: "Gum",
 		buttons: ["Gum", "Socks", "Pillows"]
 	},
 	{
 		id: 8,
-		img: "images/zoom/chair.jpg",
+		img: "unzooom/images/zoom/chair.jpg",
 		title: "Chair",
 		buttons: ["Towel", "Chair", "Spoon"]
 	},
 	{
 		id: 9,
-		img: "images/zoom/umbrella.jpg",
+		img: "unzooom/images/zoom/umbrella.jpg",
 		title: "Umbrella",
 		buttons: ["Umbrella", "Shirt", "Mountain"]
 	},
 	{
 		id: 10,
-		img: "images/zoom/racket.jpg",
+		img: "unzooom/images/zoom/racket.jpg",
 		title: "Racket",
 		buttons: ["Tire", "Racket", "Mouse"]
 	},
 	{
 		id: 11,
-		img: "images/zoom/fish.jpg",
+		img: "unzooom/images/zoom/fish.jpg",
 		title: "Fish",
 		buttons: ["Fish", "Hair", "Ring"]
 	},
 	{
 		id: 12,
-		img: "images/zoom/toothbrush.jpg",
+		img: "unzooom/images/zoom/toothbrush.jpg",
 		title: "Toothbrush",
 		buttons: ["Shoes", "Toothbrush", "Poster"]
 	},
 	{
 		id: 13,
-		img: "images/zoom/boots.jpg",
+		img: "unzooom/images/zoom/boots.jpg",
 		title: "Boots",
 		buttons: ["Bag", "Dress", "boots"]
 	},
 	{
 		id: 14,
-		img: "images/zoom/gingerbread.jpg",
+		img: "unzooom/images/zoom/gingerbread.jpg",
 		title: "Gingerbread",
 		buttons: ["Gingerbread", "Cake", "Sugar"]
 	},
 	{
 		id: 15,
-		img: "images/zoom/cotton.jpg",
+		img: "unzooom/images/zoom/cotton.jpg",
 		title: "Cotton",
 		buttons: ["Ice", "Cloud", "Cotton"]
 	},
 	{
 		id: 16,
-		img: "images/zoom/coral.jpg",
+		img: "unzooom/images/zoom/coral.jpg",
 		title: "Coral",
 		buttons: ["Painting", "Magazine", "Coral"]
 	},
 	{
 		id: 17,
-		img: "images/zoom/snow.jpg",
+		img: "unzooom/images/zoom/snow.jpg",
 		title: "Snow",
 		buttons: ["Snow", "Salt", "Sugar"]
 	},
 	{
 		id: 18,
-		img: "images/zoom/grasshopper.jpg",
+		img: "unzooom/images/zoom/grasshopper.jpg",
 		title: "Grasshopper",
 		buttons: ["Leaf", "Grasshopper", "Icing"]
 	},
 	{
 		id: 19,
-		img: "images/zoom/grasshopper.jpg",
+		img: "unzooom/images/zoom/grasshopper.jpg",
 		title: "Grasshopper",
 		buttons: ["Leaf", "Grasshopper", "Icing"]
 	},
 	{
 		id: 20,
-		img: "images/zoom/chocolate.jpg",
+		img: "unzooom/images/zoom/chocolate.jpg",
 		title: "Chocolate",
 		buttons: ["Chocolate", "Sugar", "Skin"]
 	},
 	{
 		id: 21,
-		img: "images/zoom/banana.jpg",
+		img: "unzooom/images/zoom/banana.jpg",
 		title: "Banana",
 		buttons: ["Umbrella", "Banana", "Cake"]
 	},
 	{
 		id: 22,
-		img: "images/zoom/dice.jpg",
+		img: "unzooom/images/zoom/dice.jpg",
 		title: "Dice",
 		buttons: ["Dice", "Ice", "Sugar"]
 	},
 	{
 		id: 23,
-		img: "images/zoom/pizza.jpg",
+		img: "unzooom/images/zoom/pizza.jpg",
 		title: "Pizza",
 		buttons: ["Tomato", "Ketchup", "Pizza"]
 	},
 	{
 		id: 24,
-		img: "images/zoom/basket.jpg",
+		img: "unzooom/images/zoom/basket.jpg",
 		title: "Basket",
 		buttons: ["Basket", "Rug", "Branch"]
 	},
 	{
 		id: 25,
-		img: "images/zoom/bread.jpg",
+		img: "unzooom/images/zoom/bread.jpg",
 		title: "Bread",
 		buttons: ["Bread", "Basket", "Chocolate"]
 	},
 	{
 		id: 26,
-		img: "images/zoom/candle.jpg",
+		img: "unzooom/images/zoom/candle.jpg",
 		title: "Candle",
 		buttons: ["Wand", "Pole", "Candle"]
 	}
