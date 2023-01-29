f463a9d62b42fa89911666e377cc8bd53ba3ea86
Poi <65627716+edmgarcia@users.noreply.github.com>
2023-01-29 12:07:34 -0500

    Update makePokeCard.js

--- pokemon/js/makePokeCard.js
+++ pokemon/js/makePokeCard.js
@@ -8,31 +8,32 @@ const makePokeCard = (pokeProfile) => {
 		pokedexList.classList.add("pokedex--list");
 
 		pokedexList.innerHTML +=	`<div class="card" id="${poke.name}">
-										<div class="card--inner" style="background-image: url(images/${poke.cardBg});">
-											<div class="card--header">
-												<div class="header--profile">
-													<h1 class="profile--title">${poke.name}</h1>
-													<h1 class="profile--title">XP ${poke.expi}</h1>
-												</div>
-												<div class="header--image-wrapper"><img src="${poke.image}" class="card-image"></div>
-											</div>
-											<div class="card--main">
-												<div class="main--physical align-center">
-													<div class="capsule">Height: <b>${poke.height}"</b></div> 
-													<div class="capsule">Weight: <b>${poke.weight}</b> lbs</div>
-												</div>
-												<p class="poke-desc">${poke.desc[3].flavor_text.replace("\f", " ").trim()}</p>
-											</div>
-											<hr>
-											<div class="card--footer">
-												<div class="footer--abilities align-center">
-													<p class="label">Abilities</p>
-													<div class="capsule ${poke.types[0].type.name}"><b>${poke.abilities[0].ability.name}</b></div> 
-													${poke.abilities[1] ? `<div class="capsule ${poke.types[0].type.name}"><b>${poke.abilities[1].ability.name}</b></div>`:""}
-												</div>
-											</div>
-										</div>
-									</div>`;
+							<div class="card--pokemon-id"><i class="fa fa-hashtag"></i> ${poke.id}</div>
+							<div class="card--inner" style="background-image: url(images/${poke.cardBg});">
+								<div class="card--header">
+									<div class="header--profile">
+										<h1 class="profile--title">${poke.name}</h1>
+										<h1 class="profile--title">XP ${poke.expi}</h1>
+									</div>
+									<div class="header--image-wrapper"><img src="${poke.image}" class="card-image"></div>
+								</div>
+								<div class="card--main">
+									<div class="main--physical align-center">
+										<div class="capsule">Height: <b>${poke.height}"</b></div> 
+										<div class="capsule">Weight: <b>${poke.weight}</b> lbs</div>
+									</div>
+									<p class="poke-desc">${poke.desc[3].flavor_text.replace("\f", " ").trim()}</p>
+								</div>
+								<hr>
+								<div class="card--footer">
+									<div class="footer--abilities align-center">
+										<p class="label">Abilities</p>
+										<div class="capsule ${poke.types[0].type.name}"><b>${poke.abilities[0].ability.name}</b></div> 
+										${poke.abilities[1] ? `<div class="capsule ${poke.types[0].type.name}"><b>${poke.abilities[1].ability.name}</b></div>`:""}
+									</div>
+								</div>
+							</div>
+						</div>`;
 	});
 	
 	pokedexElem.insertAdjacentElement("beforeend", pokedexList);
